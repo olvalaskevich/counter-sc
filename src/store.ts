bc3fa16bca72fa394d636460f1a4bf46f0120abb
olvalaskevich <olvol269@gmail.com>
2024-02-06 00:03:23 +0300

    add preload state for localstorage

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,13 @@
 import {combineReducers, createStore} from "redux";
 import {counterReducer} from "./counter-reducer";
 
+let preloadedState=JSON.parse(localStorage.getItem("counter") as string)
 
 const rootReducer=combineReducers({
     counter:counterReducer
 })
-
-let preloadedState=JSON.parse(localStorage.getItem("counter") as string)
 export const store=createStore(rootReducer,preloadedState)
-
 store.subscribe(()=>{
-    localStorage.setItem("counter", JSON.stringify(store.getState().counter))
+    localStorage.setItem("counter", JSON.stringify(store.getState()))
 })
-
 export type RootStateType=ReturnType<typeof rootReducer>
